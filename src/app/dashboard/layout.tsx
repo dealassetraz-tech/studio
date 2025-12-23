ad9c4a80a61caa1acf8eeed1c338690450541ecc
Om Prasath <omprasathom2@gmail.com>
2025-12-23 09:47:36 +0000

    change the white color back ground to the hex code related color

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -8,7 +8,7 @@ export default function DashboardLayout({
   return (
     <div className="flex min-h-screen">
       <Sidebar />
-      <main className="flex-1 p-8 bg-gray-100">
+      <main className="flex-1 p-8 bg-background">
         {children}
       </main>
     </div>
