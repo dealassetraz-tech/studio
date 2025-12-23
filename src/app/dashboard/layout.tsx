170de44cb933f5e349e1f7ed9549728121d78869
Om Prasath <omprasathom2@gmail.com>
2025-12-23 09:45:33 +0000

    change the back ground color

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -8,7 +8,7 @@ export default function DashboardLayout({
   return (
     <div className="flex min-h-screen">
       <Sidebar />
-      <main className="flex-1 p-8 bg-gray-100 dark:bg-zinc-900/50">
+      <main className="flex-1 p-8 bg-gray-100">
         {children}
       </main>
     </div>
