82a81129e555fc4a5396baab2503d0007547119e
Om Prasath <omprasathom2@gmail.com>
2025-12-24 09:29:57 +0000

    remove all the code which is wwwritten for the assetraz uk website

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,7 +1,6 @@
+'use client';
 import { config } from 'dotenv';
 config();
 
 import '@/ai/flows/generate-initial-codebase-from-prompt.ts';
 import '@/ai/flows/enhance-ui-screenshot.ts';
-import '@/ai/flows/verify-property.ts';
-import '@/ai/flows/check-subscription-status.ts';
